731e709b786defb60831564d03137c8d03ca34d9
lalitha <lalithapelluri6@gmail.com>
2019-04-05 22:35:57 -0700

    final code

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
+var mysql = require('mysql');
 
-// Require mysql
-var mysql = require("mysql");
+var connection;
 
-// Set up our connection information
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
-// Connect to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
@@ -23,5 +21,4 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 });
 
-// Export connection
 module.exports = connection;
